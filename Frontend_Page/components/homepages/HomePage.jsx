3f4c91550ea2512148f921438cc949d33097473a
alice duong <thachthao.8401@gmail.com>
2023-09-09 09:43:18 -0400

    2nd_commit

---
+++ Frontend_Page/components/homepages/HomePage.jsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css'
+
+const HomePage = (props) => {
+  return (
+    <div className={styles.bigFrame}>
+      <div className={styles.symBol}>
+        
+      </div>
+
+    </div>
+  )
+}
+export default HomePage
